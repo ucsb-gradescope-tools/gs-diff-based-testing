cd1a761479ecb86f174e3a8b6e2b5b3310654434
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-22 13:37:27 -0700

    better error handling when combining results

--- combine-results-json.py
+++ combine-results-json.py
@@ -9,29 +9,34 @@ import textwrap
 import os
 import sys
 
-fallback_message = '''
+
+fallback_message = textwrap.dedent('''\
+****************************************************
 There may be a problem with your submission
 that the autograder system wasn't anticipating, 
 so there is no way to give a helpful error message.
 
-There also may be a problem with the autograder configuration.
-Please consult your instructor for assistance, and
-show them this error message.
-'''
+There also may be a problem with autograder itself
+Please consult your TA or instructor for assistance, 
+and show them this error message.
 
-         
+****************************************************
+''')
+                                   
+                                   
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Combine separate Gradescope results.json files into one',
-        formatter_class=argparse.RawTextHelpFormatter)
-
-    parser.add_argument('jsonfiles', nargs='+',
-    help= textwrap.dedent('''\
-      multiple results.json files, e.g. results1.json results2.json etc.
+    parser = argparse.ArgumentParser(description=\
+                                     'Combine separate Gradescope results.json files into one',
+                                     formatter_class=argparse.RawTextHelpFormatter)
     
-      These might come from different phases, e.g. from a diff-based testing phase
-      followed by a junit phase, followed by another diff-based testing phase.
-      '''))
+    parser.add_argument('jsonfiles', nargs='+',
+                        help= textwrap.dedent('''\
+                        multiple results.json files, e.g. results1.json results2.json etc.
+                        
+                        These might come from different phases, e.g. from a diff-based testing phase
+                        followed by a junit phase, followed by another diff-based testing phase.
+                        '''))
 
     parser.add_argument('--outputfile', '-o',  default="results.json")
     parser.add_argument('--verbose', '-v', action='count', default=0)
@@ -41,18 +46,18 @@ if __name__ == "__main__":
     results = {"tests":[]}
     results_objects = []
 
-    for infile in args.jsonfiles:
-      if (not os.path.isfile(infile)):
-        haltWithError("ERROR: the inputfile " + infile + " does not exist")
-      with open(infile,'r') as infile:
+    for infile_name in args.jsonfiles:
+      if (not os.path.isfile(infile_name)):
+        haltWithError("ERROR: the inputfile " + infile_name + " does not exist")
+      with open(infile_name,'r') as infile:
         try:
             contents = json.load(infile)
             print("contents="+contents)
         except:            
             the_test = {"name": "TEST HARNESS ERROR",
-                                    "max_score": 1,
-                                    "score": 0  ,
-                                    "output": fallback_message}
+                        "max_score": 1,
+                        "score": 0  ,
+                        "output": fallback_message + "Problem file: " + infile_name}
             contents = {"tests" : [ the_test ] }
                         
         results_objects.append(contents)
